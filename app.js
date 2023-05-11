ba4080b8bf1da5f091935414eed49606119d32f2
FirdeusGj <firdegjepali@gmail.com>
2023-05-11 17:07:56 +0200

    fixed the error

--- app.js
+++ app.js
@@ -32,16 +32,16 @@ const successCallback = (position) => {
   .then((data) => {
     let country = data[0].local_names.eu
     fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=22a10d8913927bd9b9c38ed8d772ea60`
-        )
-        .then((response) => response.json())
-        .then((data) => {
-            let weather1 = data.weather[0].main
-            let temp = Math.floor(data.main.temp + -273.1)
-            locationNav.innerHTML = `${country} | ${weather1} | ${temp}°C`
-        })
-  })
-};
+      `https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=22a10d8913927bd9b9c38ed8d772ea60`
+      )
+      .then((response) => response.json())
+      .then((data) => {
+        let weather1 = data.weather[0].main
+        let temp = Math.floor(data.main.temp + -273.1)
+          locationNav.innerHTML = `${country} | ${weather1} | ${temp}°C`
+      })
+    }).catch((error) => error = locationNav.innerHTML = `Location error`)
+  };
 const errorCallback = (error) => {
   console.log(error);
 };
